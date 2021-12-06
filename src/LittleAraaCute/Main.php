17bd761d489d12ef71b81e2946266027f39526e3
Araa <79429172+ItzStarlxy@users.noreply.github.com>
2021-12-06 21:43:04 +0800

    Update to API 4.0.0

--- src/LittleAraaCute/Main.php
+++ src/LittleAraaCute/Main.php
@@ -1,1 +1,30 @@
+<?php
 
+namespace LittleAraaCute\OnlyProxyJoin;
+
+use pocketmine\Server;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+use pocketmine\event\player\PlayerPreLoginEvent;
+
+class Main extends PluginBase implements Listener {
+	
+	public function onEnable(): void{
+		$this->getLogger()->info("Plugin has Enabled");
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		@mkdir($this->getDataFolder());
+        $this->saveResource("config.yml");
+        $this->getConfig = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+	}
+	
+	public function onPreLogin(PlayerPreLoginEvent $event){
+		$player = $event->getPlayer();
+		
+		if($player->getNetworkSession()->getIp() !== $this->getConfig()->get("proxy-ip")){
+		$event->setKickMessage($this->getConfig()->get("kick-message"));
+		$event->setCancelled(true);
+	}
+}
+}
